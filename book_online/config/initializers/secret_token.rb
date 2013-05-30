8871100098377d9b0d71d14b2778a6036ebced43
fujunfeng <fujunfeng@gmail.com>
2013-05-30 17:20:49 +0800

    init book_online project

---
+++ book_online/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookOnline::Application.config.secret_token = 'c181afa4603c281f9d9e1d068401acb0a41477b6b6ec958326da73fa9a64fdf353e1fd99c500648741ed12e4f3b4154015cffa5fcb7db164f4db5b29e955d6b2'
