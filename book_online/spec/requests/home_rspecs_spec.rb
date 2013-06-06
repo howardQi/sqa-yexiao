7c8c9db848186b1b1ad1f00d6cbd2b709202d85e
fujunfeng <fujunfeng@gmail.com>
2013-06-06 19:29:22 +0800

    delete public index page

--- book_online/spec/requests/home_rspecs_spec.rb
+++ book_online/spec/requests/home_rspecs_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "HomeRspecs" do
   describe "Home Page" do
     it "should show welcome words" do
-      visit "/home/index"
+      visit "/"
       page.should have_content("welcome!")
     end
   end
